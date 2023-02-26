e2725711bfdaa165be3cc13e2fefb67262abb040
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-02-26 18:32:30 -0500

    ✨ single script to build everything

---
+++ scripts/00_build.jl
@@ -0,0 +1,3 @@
+include(joinpath(pwd(), ".typesetter", "scripts", "01_metadata.jl"))
+include(joinpath(pwd(), ".typesetter", "scripts", "02_folders.jl"))
+include(joinpath(pwd(), ".typesetter", "scripts", "03_build.jl"))
